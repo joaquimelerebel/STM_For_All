c5b18fd10a0340521e930effa831e19801276511
KekeLinux <kellian.cottart@edu.ece.fr>
2022-02-08 10:17:41 +0100

    feat: saving file in a subfolder

--- docker_domain/flask/app/index.py
+++ docker_domain/flask/app/index.py
@@ -7,19 +7,23 @@ from PIL import Image
 from flask import Flask, flash, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = '/home/dodge/Documents/PPE/STM_For_All/docker_domain/flask/app/data'
-ALLOWED_EXTENSIONS = [' ', '.npy']
-  
+UPLOAD_FOLDER = './data'
+ALLOWED_EXTENSIONS = ['', 'npy']
+
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'ww55z6e98a+f32h547r8e7'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['ALLOWED_EXTENSIONS'] = ALLOWED_EXTENSIONS
 
+
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS'] 
+           filename.rsplit('.', 1)[1].lower(
+           ) in app.config['ALLOWED_EXTENSIONS']
 
-@app.route("/", methods=['GET', 'POST'])   
+
+@app.route("/", methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
@@ -38,9 +42,22 @@ def upload_file():
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             print('Saved in ', os.path.join(app.config['UPLOAD_FOLDER']))
-            return redirect(url_for('download_file', name=filename))
+            return redirect(url_for('upload_file', name="/watch"))
+        else:
+            flash('Wrong extension. Allowed extensions : ' +
+                  str(ALLOWED_EXTENSIONS).strip('[]'))
+            print('Wrong extension. Allowed extensions : ' +
+                  str(ALLOWED_EXTENSIONS).strip('[]'))
+            return redirect(request.url)
+
     return render_template("main.html")
+
 
 @app.route("/watch", methods=['GET', 'POST'])
 def watch_file():
     return '<h1> welcome to watch </h1>'
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
