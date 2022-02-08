c214c09b6fc49ecc5e8f09594b07617456eceda3
KekeLinux <kellian.cottart@edu.ece.fr>
2022-02-08 16:12:58 +0100

    feat: incorporating binMST to webserver

--- docker_domain/flask/app/index.py
+++ docker_domain/flask/app/index.py
@@ -1,15 +1,16 @@
 from distutils.log import error
+
 import os
 import re
-
+import sys
+import struct
 from numpy import asarray, zeros, float64, array, load
 from PIL import Image
-
 from flask import Flask, flash, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = './data'
-ALLOWED_EXTENSIONS = ['mst', 'npy']
+ALLOWED_EXTENSIONS = ['bst', 'mst', 'npy']
 OUTPUT_FOLDER = './static/img/results/'
 
 app = Flask(__name__)
@@ -69,6 +70,11 @@ def watch_file(file):
 
     elif (file.endswith(('.mst'))):
         path = file_read(file)
+        return render_template("image.html", imgpath=path)
+    elif (file.endswith(('.bst'))):
+        path = custom_read(file)
+        if path == 0:
+            return render_template("uploadFile.html")
         return render_template("image.html", imgpath=path)
     else:
         print("Wrong parameters")
@@ -134,6 +140,68 @@ def file_read(file):
     if(isinstance(file, str)):
         f.close()
     return path
+
+
+def custom_read(file):
+    try:
+        with open("./data/" + file, "rb") as f:
+            filecontent = f.read()
+    except IOError as err:
+        sys.stderr.write(f"file does not exists : {err=}")
+        flash("File does not exists : ", err)
+        return 0
+
+    try:
+        if(len(filecontent) < 32):
+            raise IOError("file is too short")
+        b_header = filecontent[0:32]
+        # read the header
+        mst = b_header[0:3].decode("ascii")
+        if(mst != "MST"):
+            raise IOError("not a MST file type")
+        version = struct.unpack(">h", b_header[3:5])[0]
+        patch = struct.unpack(">h", b_header[5:7])[0]
+        point_length = struct.unpack(">B", b_header[7:8])[0]
+        height = struct.unpack(">i", b_header[8:12])[0]
+        width = struct.unpack(">i", b_header[12:16])[0]
+        metadata = b_header[16:32]
+        point_byte_len = int((2**point_length)/8)
+        supposed_len = width*height*point_byte_len
+        if(len(filecontent[32:]) != supposed_len):
+            raise IOError("file not the supposed length")
+
+        # TODO to change according to the file format point size
+        data = zeros((height, width), dtype=float64)
+        for i in range(0, supposed_len, point_byte_len):
+            point = struct.unpack(
+                ">d", filecontent[32+i: 32+i+point_byte_len])[0]
+            data[int((i/point_byte_len)/width),
+                 int(i/point_byte_len) % width] = point
+
+        # creating our array to have our image
+        output = zeros([height, width, 3], dtype=float64)
+        value = data*255/5
+        output = array([value, value, value]).transpose()
+
+        # importing image
+        img = Image.fromarray(output, 'RGB')
+        # if there is no output folder, create one
+        if not os.path.exists(app.config['OUTPUT_FOLDER']):
+            os.makedirs(app.config['OUTPUT_FOLDER'])
+
+        # set the path and save the image in it
+        path = app.config['OUTPUT_FOLDER'] + file[:-4] + '.jpg'
+        img.save(path, 'JPEG')
+        return path
+    except IOError as err:
+        sys.stderr.write(f"file does not corresponds to the format : {err=}")
+        flash('File does not corresponds to the format :', err)
+        return 0
+
+    except ValueError as err:
+        sys.stderr.write(f"could not convert data to right format : {err=}")
+        flash('Could not convert data to right format  :', err)
+        return 0
 
 
 if __name__ == "__main__":
--- docker_domain/src_show/display.py
+++ docker_domain/src_show/display.py
@@ -1,66 +1,67 @@
 import sys
 import re
-import struct 
+import struct
 
 from numpy import asarray, zeros, float64, array, load
 from PIL import Image
 
 
-def readMSTfile(filename) :
-        try :
-            with open(filename, "rb") as f :
-                filecontent = f.read();
-        except IOError as err :
-            sys.stderr.write( f"file does not exists : {err=}" );
-            return 0;
+def readMSTfile(filename):
+    try:
+        with open(filename, "rb") as f:
+            filecontent = f.read()
+    except IOError as err:
+        sys.stderr.write(f"file does not exists : {err=}")
+        return 0
 
-        try : 
-            if( len(filecontent) < 32 ) : 
-                raise IOError("file is too short");
-            b_header = filecontent[0:32];
-            # read the header
-            mst = b_header[0:3].decode("ascii");
-            if(mst != "MST") :
-                raise IOError("not a MST file type")
+    try:
+        if(len(filecontent) < 32):
+            raise IOError("file is too short")
+        b_header = filecontent[0:32]
+        # read the header
+        mst = b_header[0:3].decode("ascii")
+        if(mst != "MST"):
+            raise IOError("not a MST file type")
 
-            version = struct.unpack(">h", b_header[3:5])[0];
-            patch = struct.unpack(">h", b_header[5:7])[0];
-            point_length = struct.unpack(">B", b_header[7:8])[0];
-            height = struct.unpack(">i", b_header[8:12])[0];
-            width = struct.unpack(">i", b_header[12:16])[0];
-            metadata = b_header[16:32];
-            
-            point_byte_len = int((2**point_length)/8);
-            supposed_len = width*height*point_byte_len;
-            if( len(filecontent[32:] ) != supposed_len ) :
-                raise IOError("file not the supposed length");
-            
-            #TODO to change according to the file format point size
-            data = zeros((height, width), dtype=float64)
-            for i in range(0, supposed_len, point_byte_len):
-                point = struct.unpack(">d", filecontent[32+i: 32+i+point_byte_len])[0];
-                data[int((i/point_byte_len)/width), int(i/point_byte_len)%width] = point;
+        version = struct.unpack(">h", b_header[3:5])[0]
+        patch = struct.unpack(">h", b_header[5:7])[0]
+        point_length = struct.unpack(">B", b_header[7:8])[0]
+        height = struct.unpack(">i", b_header[8:12])[0]
+        width = struct.unpack(">i", b_header[12:16])[0]
+        metadata = b_header[16:32]
 
-            return (data, height, width)
-        except IOError as err :
-            sys.stderr.write( f"file does not corresponds to the format : {err=}" );
-            return 0;
+        point_byte_len = int((2**point_length)/8)
+        supposed_len = width*height*point_byte_len
+        if(len(filecontent[32:]) != supposed_len):
+            raise IOError("file not the supposed length")
 
-        except ValueError as err :
-            sys.stderr.write( f"could not convert data to right format : {err=}" );
-            return 0;
+        # TODO to change according to the file format point size
+        data = zeros((height, width), dtype=float64)
+        for i in range(0, supposed_len, point_byte_len):
+            point = struct.unpack(
+                ">d", filecontent[32+i: 32+i+point_byte_len])[0]
+            data[int((i/point_byte_len)/width),
+                 int(i/point_byte_len) % width] = point
 
+        return (data, height, width)
+    except IOError as err:
+        sys.stderr.write(f"file does not corresponds to the format : {err=}")
+        return 0
+
+    except ValueError as err:
+        sys.stderr.write(f"could not convert data to right format : {err=}")
+        return 0
 
 
 def main(filename, binary, output):
 
-    if ("-bin" in binary ):
+    if ("-bin" in binary):
         if (binary == "-binNP"):
             file = load(filename)
-            height, width = file.shape 
-            
-        if (binary == "-bin") :
-            file, height, width = readMSTfile(filename);
+            height, width = file.shape
+
+        if (binary == "-bin"):
+            file, height, width = readMSTfile(filename)
 
         # creating our array to have our image
         data = zeros((height, width, 3), dtype=float64)
@@ -72,9 +73,7 @@ def main(filename, binary, output):
         img.save(output)
         img.show(output)
 
-
-
-    elif (binary =="-f"):
+    elif (binary == "-f"):
         # try opening output_filename, if there is none, let's do it in stdout
         if(isinstance(filename, str)):
             f = open(filename, "r")
@@ -92,7 +91,8 @@ def main(filename, binary, output):
 
         for h in range(0, height):
             line = f.readline()
-            value = asarray(line.replace(" ", "").split(","), float64)[0:width]*(255/5)
+            value = asarray(line.replace(" ", "").split(","), float64)[
+                0:width]*(255/5)
             data[h] = array([value, value, value]).transpose()
 
         # image
@@ -105,8 +105,9 @@ def main(filename, binary, output):
     else:
         print("Wrong parameters")
 
+
 if(__name__ == "__main__"):
-    if len( sys.argv ) <= 2 :
-        print( "Not enough arguments" )
+    if len(sys.argv) <= 2:
+        print("Not enough arguments")
         exit()
     main(sys.argv[1], sys.argv[2], sys.argv[3])
